65b4dcd70a6cb71cd95681212364bd4129a3b8cd
Mu Wang <wangmu@shanghaitech.edu.cn>
2023-06-27 22:40:43 +0800

    update pdbqt2pdb.py

--- pdbqt2pdb.py
+++ pdbqt2pdb.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 """
 Convert pdbqt files (of ligands or receptors) to pdb files by openbabel.
 This script will serach pdbqt files under /path/to/pdbqt, convert and save to the same directory.
@@ -26,11 +27,13 @@ pdbqt_path = args.pdbqt_path
 
 
 def main():
+    if not os.path.exists(pdbqt_path):
+        raise FileNotFoundError(f'{pdbqt_path} not found')
     for pdbqt_file in glob(f'{pdbqt_path}/*.pdbqt'):
         pdb_file = pdbqt_file.replace('.pdbqt', '.pdb')
         cmd =f'obabel -ipdbqt {pdbqt_file} -opdb -O {pdb_file}'
         print(cmd)
-        os.system(cmd)
+        #os.system(cmd)
 
 
 if __name__ == '__main__':
